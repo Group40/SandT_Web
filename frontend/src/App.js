d4fa2030bf6733cf4620d7c7438367dd99c2aa2d
piyumaldk <piyumaldk@gmail.com>
2020-07-27 12:41:44 +0530

    Admin Login Ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import AdminRoutes from "./Pages/Admin/AdminRoutes";
+import AdminLogin from "./Pages/Admin/AdminLogin";
 
 class App extends Component {
   render(){
     return (
       <React.Fragment>
         <Router>
-        
+          <Route path="/adminlogin" component={AdminLogin}/>
           <Route path="/admin" component={AdminRoutes}/>
         </Router>
         </React.Fragment>
