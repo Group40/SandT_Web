ede2cdd981f4af9797a5d465600e787eca3012f4
piyumaldk <piyumaldk@gmail.com>
2020-08-07 19:53:57 +0530

    Age group upgrade

--- backend/src/main/java/com/api/model/Course.java
+++ backend/src/main/java/com/api/model/Course.java
@@ -9,7 +9,8 @@ import lombok.ToString;
 public class Course {
     private String id;
     private String name; 
-	private String ageGroup; 
+	private String ageGroupMin;
+	private String ageGroupMax; 
 	private String price;
 	private String location;
 	private String description;
@@ -26,11 +27,17 @@ public class Course {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getAgeGroup() {
-		return ageGroup;
+	public String getAgeGroupMin() {
+		return ageGroupMin;
 	}
-	public void setAgeGroup(String ageGroup) {
-		this.ageGroup = ageGroup;
+	public void setAgeGroupMin(String ageGroupMin) {
+		this.ageGroupMin = ageGroupMin;
+	}
+	public String getAgeGroupMax() {
+		return ageGroupMax;
+	}
+	public void setAgeGroupMax(String ageGroupMax) {
+		this.ageGroupMax = ageGroupMax;
 	}
 	public String getPrice() {
 		return price;
