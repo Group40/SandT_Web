b2f51dbe737743e112b48cd096c9696a46655d16
piyumaldk <piyumaldk@gmail.com>
2020-08-10 19:20:28 +0530

    Fix notification

--- frontend/src/Pages/Admin/Notification.js
+++ frontend/src/Pages/Admin/Notification.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react'
 import axios from "axios";
 import { Table, Spinner, Container } from "reactstrap";
 import AdminNav from "../../Components/AdminNav.component";
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCalendarAlt, faStar, faMapMarkerAlt, faEdit, faDollarSign } from "@fortawesome/free-solid-svg-icons";
+
 export default class Notification extends Component {
 
     constructor(props) {
@@ -19,7 +17,7 @@ export default class Notification extends Component {
         await axios.get("http://localhost:8080/findAllNotifications")
         .then(res => {
             this.setState({ 
-                NotificationList: res.data,
+                NotificationList: res.data.reverse(),
                 loading: false
             })
         }) 
@@ -41,95 +39,92 @@ export default class Notification extends Component {
             <AdminNav/> 
             <Container>
                 
-                    <div style={{ marginTop: "20px" }}>
-                        <Table>
+                <div style={{ marginTop: "20px" }}>
+                    <Table>
                             <tbody>
-                        {this.state.NotificationList.map(function(notification, index) {
-                            return (
-                                <React.Fragment key={index}>
-                                    <tr>
-                    <td>
-                                        
-                                    {(notification.authorType == "3")
-                ?
-                //Admin----------------------------------------------
-                (notification.eventDate == null)
-                    ?
-                    //Course
-                    
-                        notification.authorName +
-                            " who has access as an admin " +
-                            notification.nameType +
-                            " '" +
-                            notification.name +
-                            "' from " +
-                            notification.authorMail +
-                            " on " +
-                            notification.date.substring(0, 10) +
-                            " at " +
-                            notification.date.substring(11, 20)
-                    :
-                    //Event
-                    
-                    notification.authorName +
-                            " who has access as an admin " +
-                            notification.nameType +
-                            " '" +
-                            notification.name +
-                            "' (event date : " +
-                            notification.eventDate +
-                            ") from " +
-                            notification.authorMail +
-                            " on " +
-                            notification.date.substring(0, 10) +
-                            " at " +
-                            notification.date.substring(11, 20)
-                :
-                //Crew Member----------------------------------------
-                (notification.eventDate == null)
-                    ?
-                    //Course
-                    
-                    notification.authorName +
-                            " who has access as a crew member " +
-                            notification.nameType +
-                            " '" +
-                            notification.name +
-                            "' (event date : " +
-                            notification.eventDate +
-                            ") from " +
-                            notification.authorMail +
-                            " on " +
-                            notification.date.substring(0, 10) +
-                            " at " +
-                            notification.date.substring(11, 20)
-                    :
-                    //Event
-                    
-                    notification.authorName +
-                            " who has access as a crew member " +
-                            notification.nameType +
-                            " '" +
-                            notification.name +
-                            "' (event date : " +
-                            notification.eventDate +
-                            ") from " +
-                            notification.authorMail +
-                            " on " +
-                            notification.date.substring(0, 10) +
-                            " at " +
-                            notification.date.substring(11, 20)}
-                        </td>
-                                          
-                                    </tr>
-                                </React.Fragment>
-                            );
-                        })}  
-                        </tbody>  
+                                {this.state.NotificationList.map(function(notification, index) {
+                                    return (
+                                        <React.Fragment key={index}>
+                                            <tr>
+                                                <td>   
+                                                    {(notification.authorType == "3")
+                                                        ?
+                                                        //Admin----------------------------------------------
+                                                        (notification.eventDate == null)
+                                                            ?
+                                                            //Course
+                                                            notification.authorName +
+                                                            " who has access as an admin " +
+                                                            notification.nameType +
+                                                            " '" +
+                                                            notification.name +
+                                                            "' from " +
+                                                            notification.authorMail +
+                                                            " on " +
+                                                            notification.date.substring(0, 10) +
+                                                            " at " +
+                                                            notification.date.substring(11, 20)
+                                                            :
+                                                            //Event
+                                                            notification.authorName +
+                                                            " who has access as an admin " +
+                                                            notification.nameType +
+                                                            " '" +
+                                                            notification.name +
+                                                            "' (event date : " +
+                                                            notification.eventDate +
+                                                            ") from " +
+                                                            notification.authorMail +
+                                                            " on " +
+                                                            notification.date.substring(0, 10) +
+                                                            " at " +
+                                                            notification.date.substring(11, 20)
+                                                        :
+                                                        //Crew Member----------------------------------------
+                                                        (notification.eventDate == null)
+                                                            ?
+                                                            //Course
+                                                            notification.authorName +
+                                                            " who has access as a crew member " +
+                                                            notification.nameType +
+                                                            " '" +
+                                                            notification.name +
+                                                            "' (event date : " +
+                                                            notification.eventDate +
+                                                            ") from " +
+                                                            notification.authorMail +
+                                                            " on " +
+                                                            notification.date.substring(0, 10) +
+                                                            " at " +
+                                                            notification.date.substring(11, 20)
+                                                            :
+                                                            //Event
+                                                            notification.authorName +
+                                                            " who has access as a crew member " +
+                                                            notification.nameType +
+                                                            " '" +
+                                                            notification.name +
+                                                            "' (event date : " +
+                                                            notification.eventDate +
+                                                            ") from " +
+                                                            notification.authorMail +
+                                                            " on " +
+                                                            notification.date.substring(0, 10) +
+                                                            " at " +
+                                                            notification.date.substring(11, 20)
+                                                    }
+                                                </td>
+                                                <td>
+
+                                                </td>     
+                                            </tr>
+                                        </React.Fragment>
+                                    );
+                                })}  
+                            </tbody>  
                         </Table>          
                     </div>  
-                
-            </Container>        
+                </Container>        
             </React.Fragment>
         )  
     }
