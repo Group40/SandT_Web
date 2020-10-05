5d91542057b332d573e407bcf637ce5e1b802176
HasiniKandage <hasinichamika96@gmail.com>
2020-10-06 02:40:33 +0530

    Removed query

--- src/main/java/com/api/model/Forum.java
+++ src/main/java/com/api/model/Forum.java
@@ -11,8 +11,8 @@ public class Forum {
     // private String eventId;
     private String title;
     private String date;
-    private String startDate;
-    private String startTime;
+    // private String startDate;
+    // private String startTime;
     private String status = "0";
 
     public String getId() {
@@ -27,13 +27,13 @@ public class Forum {
         this.status = status;
     }
 
-    public String getStartTime() {
-        return startTime;
-    }
+    // public String getStartTime() {
+    //     return startTime;
+    // }
 
-    public void setStartTime(String startTime) {
-        this.startTime = startTime;
-    }
+    // public void setStartTime(String startTime) {
+    //     this.startTime = startTime;
+    // }
 
     // public String getEventId() {
     //     return eventId;
@@ -47,13 +47,13 @@ public class Forum {
         this.id = id;
     }
 
-    public String getStartDate() {
-        return startDate;
-    }
+    // public String getStartDate() {
+    //     return startDate;
+    // }
 
-    public void setStartDateTime(String startDate) {
-        this.startDate = startDate;
-    }
+    // public void setStartDateTime(String startDate) {
+    //     this.startDate = startDate;
+    // }
 
     public String getDate() {
         return date;
--- src/main/java/com/api/repository/ForumRepository.java
+++ src/main/java/com/api/repository/ForumRepository.java
@@ -1,15 +1,16 @@
 package com.api.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.api.model.Forum;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 
 public interface ForumRepository extends MongoRepository<Forum, String> {
-    @Query(value = "{'id': '$0', 'status': '0'}")
-    void deleteById(String id);//i7u7
+    void deleteById(String id);
 
-    @Query(value = "{ 'status': '1'}", fields = "{'id': 1, 'title': 1, 'status': 1}")
-	List<Forum> findByStatus();
+    List<Forum> findByStatus(String status);
+
+    Optional<Forum> findById(String id);
+   
 }
