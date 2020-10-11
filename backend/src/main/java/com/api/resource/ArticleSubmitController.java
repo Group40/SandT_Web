3d9b56c754c05e49b54d6c7b2b92e0644d37faae
punsaraCP <punsarachamath@gmail.com>
2020-10-12 00:33:04 +0530

    Article backend

--- backend/src/main/java/com/api/resource/ArticleSubmitController.java
+++ backend/src/main/java/com/api/resource/ArticleSubmitController.java
@@ -1,2 +1,27 @@
-package com.api.resource;public class ArticleSubmitController {
+package com.api.resource;
+
+import com.api.model.Article;
+import com.api.model.Event;
+import com.api.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ArticleSubmitController {
+    @Autowired
+    private ArticleRepository repository;
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @PostMapping("/addArticle")
+    public String saveArticle(@RequestBody Article article) {
+        repository.save(article);
+        return "Added a new article";
+    }
+
+    @GetMapping("/getAllarticle")
+    public List<Article> all(){
+        return repository.findAll();
+    }
 }
